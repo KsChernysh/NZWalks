0b1a42d929a1cda2931ade412272ae48a4ad0284
oksana18ch <oksanachernysh00@gmail.com>
2022-12-29 20:51:47 +0200

    End of section 3

---
+++ NZWalks/NZWalksAPI/Profiles/RegionsProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace NZWalksAPI.Profiles
+{
+    public class RegionsProfile : Profile
+    {
+        public RegionsProfile()
+        {
+        CreateMap<Models.Domain.Region, Models.DTO.Region>()
+         .ReverseMap();
+
+
+        }
+    }
+}
---
+++ NZWalks/NZWalksAPI/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDBContext nZWalksDBContext;
+
+        public RegionRepository(NZWalksDBContext nZWalksDBContext)
+        {
+            this.nZWalksDBContext = nZWalksDBContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+         return await nZWalksDBContext.Region.ToListAsync();
+        }
+    }
+}
