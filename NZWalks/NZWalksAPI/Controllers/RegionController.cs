116c9e15e37d436e046e11feaa9d63b17348a251
oksana18ch <oksanachernysh00@gmail.com>
2023-01-03 23:00:07 +0200

    end of section 4

--- NZWalks/NZWalksAPI/Controllers/RegionController.cs
+++ NZWalks/NZWalksAPI/Controllers/RegionController.cs
@@ -18,6 +18,7 @@ namespace NZWalksAPI.Controllers
             this.mapper = mapper;
         }
         [HttpGet]
+
         public async Task<IActionResult> GetAllRegions()
         {
             var regions = await regionRepository.GetAllAsync();
@@ -42,5 +43,118 @@ namespace NZWalksAPI.Controllers
             var regionsDTO = mapper.Map<List<Models.Domain.Region>>(regions);
             return Ok(regionsDTO);
         }
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetRegionAsync")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await regionRepository.GetAsync(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            var regionDTO = mapper.Map<Models.Domain.Region>(region);
+            return Ok(regionDTO);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(Models.DTO.AddRegionRequest addRegionRequest)
+        {
+            //request(dto) to domain model
+            var region = new Models.Domain.Region()
+            {
+                Code = addRegionRequest.Code,
+                Name = addRegionRequest.Name,
+                Area = addRegionRequest.Area,
+                Lat = addRegionRequest.Lat,
+                Long = addRegionRequest.Long,
+                Population = addRegionRequest.Population,
+
+
+            };
+
+            //pass details to repository
+            region = await regionRepository.AddAsync(region);
+            // convert data into dto
+            var regionDTO = new Models.DTO.Region() {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+
+            };
+            return CreatedAtAction(nameof(GetRegionAsync), new { id = regionDTO.Id }, regionDTO);
+        }
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteRegion (Guid id)
+        {
+            // get region from DB
+          var region = await  regionRepository.DeleteAsync(id);
+
+            //if null - not found
+            if(region == null)
+            {
+                return NotFound();
+            }
+            // convert from domain to dto
+            var regionDTO = new Models.DTO.Region()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+
+            };
+            //return ok(response)
+            return Ok(regionDTO);
+        }
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateRegionAsync([FromRoute] Guid id, [FromBody] Models.DTO.UpdateRegionRequest updateRegionRequest)
+        {
+            // convert DTO to domain model
+            var region = new Models.Domain.Region()
+            {
+                Code = updateRegionRequest.Code,
+                Name = updateRegionRequest.Name,
+                Area = updateRegionRequest.Area,
+                Lat = updateRegionRequest.Lat,
+                Long = updateRegionRequest.Long,
+                Population = updateRegionRequest.Population,
+
+
+            };
+            // create update 
+          region = await  regionRepository.UpdateAsync(id, region);
+        // if null - not found
+        if(region == null)
+            {
+                return NotFound();
+            }
+            // return ok(response)
+            var regionDTO = new Models.DTO.Region()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+
+            };
+            
+            return Ok(regionDTO);
+
+        }
     }
+  
+    
 }
+    
--- NZWalks/NZWalksAPI/Repositories/RegionRepository.cs
+++ NZWalks/NZWalksAPI/Repositories/RegionRepository.cs
@@ -12,9 +12,57 @@ namespace NZWalksAPI.Repositories
         {
             this.nZWalksDBContext = nZWalksDBContext;
         }
+
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await nZWalksDBContext.AddAsync(region);
+            await nZWalksDBContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+           var region = await nZWalksDBContext.Region.FirstOrDefaultAsync(x => x.Id == id);
+           if(region == null)
+            {
+              return null;
+            }
+            //delete region
+            nZWalksDBContext.Region.Remove(region);
+           await nZWalksDBContext.SaveChangesAsync();
+            return region;
+
+        }
+
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
          return await nZWalksDBContext.Region.ToListAsync();
+        }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+            return await nZWalksDBContext.Region.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Region> UpdateAsync(Guid id,Region region)
+        {
+
+         var exregion = await  nZWalksDBContext.Region.FirstOrDefaultAsync(x => x.Id == id);
+            if(exregion == null)
+            {
+                return null;
+            }
+            exregion.Code = region.Code;
+            exregion.Name = region.Name;
+            exregion.Population = region.Population;
+            exregion.Area = region.Area;
+            exregion.Lat = region.Lat;
+            exregion.Long = region.Long;
+            await nZWalksDBContext.SaveChangesAsync();
+            return exregion;
+
+
         }
     }
 }
